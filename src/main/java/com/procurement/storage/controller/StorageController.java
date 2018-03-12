fcb2c1da1267be1ab726e72b17a98d452dbdba83
andreykolchev <andreykolchev@gmail.com>
2018-03-12 12:35:01 +0200

    add mapping

--- src/main/java/com/procurement/storage/controller/StorageController.java
+++ src/main/java/com/procurement/storage/controller/StorageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/storage")
 public class StorageController {
 
     private final StorageService storageService;
