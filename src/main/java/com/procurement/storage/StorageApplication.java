374a5e3ed4e8aafb5296fbc27be1ffe72bbfd470
andreykolchev <andreykolchev@gmail.com>
2018-03-26 15:22:11 +0300

    add deploy profile

--- src/main/java/com/procurement/storage/StorageApplication.java
+++ src/main/java/com/procurement/storage/StorageApplication.java
@@ -2,10 +2,10 @@ package com.procurement.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class StorageApplication {
     public static void main(final String[] args) {
         SpringApplication.run(StorageApplication.class, args);
