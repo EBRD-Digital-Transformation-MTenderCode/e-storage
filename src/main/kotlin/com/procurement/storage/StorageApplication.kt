2e414d955184cd72b8c21750866fcdf1b7d9bd04
andreykolchev <andreykolchev@gmail.com>
2018-09-21 16:53:09 +0300

    command

--- src/main/kotlin/com/procurement/storage/StorageApplication.kt
+++ src/main/kotlin/com/procurement/storage/StorageApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.storage
 import com.procurement.storage.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class StorageApplication
 
 fun main(args: Array<String>) {
