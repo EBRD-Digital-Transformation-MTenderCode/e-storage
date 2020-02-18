8ede475fd203b6c78266ee463839df1ae7de30a4
yevhenii <yevheniiskoryi@gmail.com>
2020-02-18 12:43:19 +0200

    ES-2519 added ApiResponse with Success, Fail, Incident response

---
+++ src/main/kotlin/com/procurement/storage/infrastructure/web/dto/ApiResponse.kt
@@ -0,0 +1,73 @@
+package com.procurement.storage.infrastructure.web.dto
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.procurement.storage.domain.model.enums.ResponseStatus
+import com.procurement.storage.infrastructure.bind.apiversion.ApiVersionDeserializer
+import com.procurement.storage.infrastructure.bind.apiversion.ApiVersionSerializer
+import java.time.LocalDateTime
+import java.util.*
+
+sealed class ApiResponse(
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID,
+
+    @field:JsonProperty("version") @param:JsonProperty("version") val version: ApiVersion,
+
+    @field:JsonProperty("result") @param:JsonProperty("result") val result: Any?
+) {
+    abstract val status: ResponseStatus
+}
+
+class ApiSuccessResponse(
+    @JsonDeserialize(using = ApiVersionDeserializer::class)
+    @JsonSerialize(using = ApiVersionSerializer::class)
+    version: ApiVersion,
+    id: UUID,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY) result: Any?
+) : ApiResponse(
+    version = version,
+    result = result,
+    id = id
+) {
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.SUCCESS
+}
+
+class ApiFailResponse(
+    @JsonDeserialize(using = ApiVersionDeserializer::class)
+    @JsonSerialize(using = ApiVersionSerializer::class)
+    version: ApiVersion,
+    id: UUID,
+    result: List<Error>
+) : ApiResponse(
+    version = version,
+    result = result,
+    id = id
+) {
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.FAIL
+
+    class Error(val code: String?, val description: String?)
+}
+
+class ApiIncidentResponse(
+    @JsonDeserialize(using = ApiVersionDeserializer::class)
+    @JsonSerialize(using = ApiVersionSerializer::class)
+    version: ApiVersion,
+    id: UUID,
+    result: Incident
+) : ApiResponse(
+    version = version,
+    result = result,
+    id = id
+) {
+    @field:JsonProperty("status")
+    override val status: ResponseStatus = ResponseStatus.INCIDENT
+
+    class Incident(val id: UUID, val date: LocalDateTime, val service: Service, val errors: List<Error>) {
+        class Service(val id: String, val name: String, val version: ApiVersion)
+        class Error(val code: String, val description: String, val metadata: Any?)
+    }
+}
