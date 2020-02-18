fc3ac808c9c96832ccfb94afcaae1abc9ea38484
yevhenii <yevheniiskoryi@gmail.com>
2020-02-18 12:55:02 +0200

    ES-2519 added application level dto for command 'checkRegistration'

---
+++ src/main/kotlin/com/procurement/storage/application/service/dto/CheckRegistrationParams.kt
@@ -0,0 +1,7 @@
+package com.procurement.storage.application.service.dto
+
+import com.procurement.storage.domain.model.document.DocumentId
+
+class CheckRegistrationParams(
+    val documentIds: List<DocumentId>
+)
