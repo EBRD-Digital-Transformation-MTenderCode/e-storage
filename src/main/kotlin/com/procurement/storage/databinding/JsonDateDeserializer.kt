3606577d901b97df0f9457efb39a17a7d9fca956
yevhenii <yevheniiskoryi@gmail.com>
2020-02-20 18:50:20 +0200

    ES-2519 moved DateFormatter to private class

--- src/main/kotlin/com/procurement/storage/databinding/JsonDateDeserializer.kt
+++ src/main/kotlin/com/procurement/storage/databinding/JsonDateDeserializer.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
-import com.procurement.storage.config.JsonConfig
+import com.procurement.storage.infrastructure.bind.date.DateFormatter
 import java.io.IOException
 import java.time.LocalDateTime
 
@@ -13,6 +13,6 @@ class JsonDateDeserializer : JsonDeserializer<LocalDateTime>() {
     @Throws(IOException::class, JsonProcessingException::class)
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): LocalDateTime {
         val dateTime = jsonParser.text
-        return LocalDateTime.parse(dateTime, JsonConfig.DateFormatter.formatter)
+        return LocalDateTime.parse(dateTime, DateFormatter.formatter)
     }
 }
---
+++ src/main/kotlin/com/procurement/storage/infrastructure/bind/date/DateFormatter.kt
@@ -0,0 +1,20 @@
+package com.procurement.storage.infrastructure.bind.date
+
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+
+object DateFormatter {
+    val formatter: DateTimeFormatter = DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE)
+        .appendLiteral('T')
+        .appendValue(ChronoField.HOUR_OF_DAY, 2)
+        .appendLiteral(':')
+        .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+        .optionalStart()
+        .appendLiteral(':')
+        .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+        .appendLiteral('Z')
+        .toFormatter()
+}
