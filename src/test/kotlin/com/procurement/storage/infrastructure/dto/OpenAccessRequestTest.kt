189a952d3ee3978b8cca7df89b424779a71d6fd2
yevhenii <yevheniiskoryi@gmail.com>
2020-02-20 16:03:54 +0200

    ES-2519 added mapping test for request dto for 'openAccess'

---
+++ src/test/kotlin/com/procurement/storage/infrastructure/dto/OpenAccessRequestTest.kt
@@ -0,0 +1,13 @@
+package com.procurement.storage.infrastructure.dto
+
+import com.procurement.storage.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class OpenAccessRequestTest : AbstractDTOTestBase<OpenAccessRequest>(OpenAccessRequest::class.java){
+
+
+    @Test
+    fun test(){
+        testBindingAndMapping(pathToJsonFile = "json/dto/open_access_request_full.json")
+    }
+}
