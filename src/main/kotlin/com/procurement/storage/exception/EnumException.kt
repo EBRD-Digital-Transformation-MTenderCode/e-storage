04613a6d12a02095e2be49534e2190a6d5e77dc0
yevhenii <yevheniiskoryi@gmail.com>
2020-02-24 16:15:30 +0200

    ES-2522 added message to EnumException

--- src/main/kotlin/com/procurement/storage/exception/EnumException.kt
+++ src/main/kotlin/com/procurement/storage/exception/EnumException.kt
@@ -1,7 +1,8 @@
 package com.procurement.storage.exception
 
-data class EnumException(private val enumType: String, val value: String, val values: String) : RuntimeException() {
+data class EnumException(private val enumType: String, val value: String, val values: String) : RuntimeException(
+    "Unknown value for enumType $enumType: $value, Allowed values are $values"
+) {
 
     val code: String = "00.00"
-    val msg: String = "Unknown value for enumType $enumType: $value, Allowed values are $values"
 }
