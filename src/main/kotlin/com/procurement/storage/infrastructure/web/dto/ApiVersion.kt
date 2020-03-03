e6425bc4cbee0aaf37038fffdc381986d11cb428
yevhenii <yevheniiskoryi@gmail.com>
2020-03-03 14:42:14 +0200

    ES-2522 added tryOf method

--- src/main/kotlin/com/procurement/storage/infrastructure/web/dto/ApiVersion.kt
+++ src/main/kotlin/com/procurement/storage/infrastructure/web/dto/ApiVersion.kt
@@ -1,5 +1,7 @@
 package com.procurement.storage.infrastructure.web.dto
 
+import com.procurement.storage.domain.util.Result
+
 data class ApiVersion(val major: Int, val minor: Int, val patch: Int) {
     companion object {
         fun valueOf(version: String): ApiVersion {
@@ -23,6 +25,29 @@ data class ApiVersion(val major: Int, val minor: Int, val patch: Int) {
                 0
 
             return ApiVersion(major = major, minor = minor, patch = patch)
+        }
+
+        fun tryOf(version: String): Result<ApiVersion, String> {
+            val elements = version.split(".")
+            if (elements.isEmpty() || elements.size > 3)
+                return Result.failure("Invalid value of the api version ($version).")
+
+            val major: Int = elements[0].toIntOrNull()
+                ?: return Result.failure("Invalid value of the api version ($version).")
+
+            val minor: Int = if (elements.size >= 2) {
+                elements[1].toIntOrNull()
+                    ?: return Result.failure("Invalid value of the api version ($version).")
+            } else
+                0
+
+            val patch: Int = if (elements.size == 3) {
+                elements[2].toIntOrNull()
+                    ?: return Result.failure("Invalid value of the api version ($version).")
+            } else
+                0
+
+            return Result.success(ApiVersion(major = major, minor = minor, patch = patch))
         }
     }
 
