c8e7ea4713368eb6bf9c17b97ce54b612f217c78
yevhenii <yevheniiskoryi@gmail.com>
2020-03-03 14:51:58 +0200

    ES-2522 refactored StorageService

--- src/main/kotlin/com/procurement/storage/application/service/StorageService.kt
+++ src/main/kotlin/com/procurement/storage/application/service/StorageService.kt
@@ -2,16 +2,23 @@ package com.procurement.storage.application.service
 
 import com.procurement.storage.config.UploadFileProperties
 import com.procurement.storage.dao.FileDao
+import com.procurement.storage.domain.fail.Fail
+import com.procurement.storage.domain.fail.error.ValidationErrors
 import com.procurement.storage.domain.model.document.DocumentId
-import com.procurement.storage.exception.BpeErrorException
-import com.procurement.storage.exception.ErrorType
-import com.procurement.storage.infrastructure.dto.OpenAccessResponse
+import com.procurement.storage.domain.util.Result
+import com.procurement.storage.domain.util.ValidationResult
+import com.procurement.storage.domain.util.ValidationRule
+import com.procurement.storage.domain.util.extension.getUnknownElements
+import com.procurement.storage.domain.util.extension.mapResult
+import com.procurement.storage.domain.util.extension.toSetBy
+import com.procurement.storage.domain.util.validate
+import com.procurement.storage.infrastructure.dto.OpenAccessResult
 import com.procurement.storage.utils.toLocal
 import org.springframework.stereotype.Service
 
 interface StorageService {
-    fun checkRegistration(requestDocumentIds: List<DocumentId>)
-    fun openAccess(requestDocumentIds: List<DocumentId>): List<OpenAccessResponse>
+    fun checkRegistration(requestDocumentIds: List<DocumentId>): ValidationResult<Fail>
+    fun openAccess(requestDocumentIds: List<DocumentId>): Result<List<OpenAccessResult>, Fail>
 }
 
 @Service
@@ -20,73 +27,67 @@ class StorageServiceImpl(
     private val uploadFileProperties: UploadFileProperties
 ) : StorageService {
 
-    override fun openAccess(requestDocumentIds: List<DocumentId>): List<OpenAccessResponse> {
-        val documentIds = mapRequestDocumentIdsToString(documentIds = requestDocumentIds)
+    override fun openAccess(requestDocumentIds: List<DocumentId>): Result<List<OpenAccessResult>, Fail> {
+        val documentIdsResult = validationDocumentIds(ids = requestDocumentIds)
+        if (documentIdsResult.isFail)
+            return Result.failure(documentIdsResult.error)
+
+        val documentIds = documentIdsResult.get
         val dbFiles = getDocumentsByIds(documentIds)
 
         if (dbFiles.isEmpty()) {
-            throw BpeErrorException(
-                error = ErrorType.FILES_NOT_FOUND,
-                message = "Files not found by ids $documentIds"
-            )
+            return Result.failure(ValidationErrors.DocumentsNotExisting(documentIds))
         }
-        val uniqueDbFilesId = dbFiles.asSequence()
-            .map { it.id }
-            .toSet()
-        if (uniqueDbFilesId != documentIds) {
-            throw BpeErrorException(
-                error = ErrorType.FILES_NOT_FOUND,
-                message = (documentIds - uniqueDbFilesId).toString() + " files not found"
-            )
-        }
-        return dbFiles.map { file ->
-            OpenAccessResponse(
-                id = file.id,
-                datePublished = file.datePublished!!.toLocal(),
-                uri = uploadFileProperties.path + file.id
-            )
-        }
-    }
 
-    override fun checkRegistration(requestDocumentIds: List<DocumentId>) {
+        val receivedDocumentIds = requestDocumentIds.toSet()
+        val knownDocumentIds = dbFiles.toSetBy { it.id }
+        val unknownDocumentIds = knownDocumentIds.getUnknownElements(received = receivedDocumentIds)
+        if (unknownDocumentIds.isNotEmpty())
+            return Result.failure(ValidationErrors.DocumentsNotExisting(unknownDocumentIds))
 
-        val documentIds = mapRequestDocumentIdsToString(documentIds = requestDocumentIds)
-        val dbFiles = getDocumentsByIds(documentIds)
-
-        if (dbFiles.isEmpty()) {
-            throw BpeErrorException(
-                error = ErrorType.FILES_NOT_FOUND,
-                message = "Files not found by ids $documentIds"
-            )
-        }
-        val uniqueDbFilesId = dbFiles.asSequence()
-            .map { it.id }
-            .toSet()
-        if (uniqueDbFilesId != documentIds) {
-            throw BpeErrorException(
-                error = ErrorType.FILES_NOT_FOUND,
-                message = (documentIds - uniqueDbFilesId).toString() + " files not found"
-            )
-        }
-    }
-
-    private fun getDocumentsByIds(documentIds: Set<String>) = fileDao.getAllByIds(documentIds)
-
-    private fun mapRequestDocumentIdsToString(documentIds: List<DocumentId>): Set<String> {
-        return documentIds.asSequence()
-            .map { documentId ->
-                documentId.toString().also { id ->
-                    checkIfIdIsBlank(id = id)
-                }
+        return Result.success(
+            dbFiles.map { file ->
+                OpenAccessResult(
+                    id = file.id,
+                    datePublished = file.datePublished!!.toLocal(),
+                    uri = uploadFileProperties.path + file.id
+                )
             }
-            .toSet()
+        )
     }
 
-    private fun checkIfIdIsBlank(id: String) {
-        if (id.isBlank())
-            throw BpeErrorException(
-                error = ErrorType.INVALID_ID,
-                message = "The id = $id in the document is blank"
-            )
+    override fun checkRegistration(requestDocumentIds: List<DocumentId>): ValidationResult<Fail> {
+
+        val documentIdsResult = validationDocumentIds(ids = requestDocumentIds)
+        if (documentIdsResult.isFail)
+            return ValidationResult.error(documentIdsResult.error)
+
+        val documentIds = documentIdsResult.get
+        val dbFiles = getDocumentsByIds(documentIds)
+
+        if (dbFiles.isEmpty()) {
+            return ValidationResult.error(ValidationErrors.DocumentsNotExisting(documentIds))
+        }
+
+        val receivedDocumentIds = requestDocumentIds.toSet()
+        val knownDocumentIds = dbFiles.toSetBy { it.id }
+        val unknownDocumentIds = knownDocumentIds.getUnknownElements(received = receivedDocumentIds)
+        if (unknownDocumentIds.isNotEmpty())
+            return ValidationResult.error(ValidationErrors.DocumentsNotExisting(unknownDocumentIds))
+
+        return ValidationResult.ok()
     }
+
+    private fun getDocumentsByIds(documentIds: List<String>) = fileDao.getAllByIds(documentIds.toSet())
+
+    private fun validationDocumentIds(ids: List<DocumentId>): Result<List<String>, ValidationErrors> =
+        ids.mapResult { id -> id validate onBlank }
+
+    private val onBlank =
+        ValidationRule { id: DocumentId ->
+            if (id.isBlank())
+                ValidationResult.error(ValidationErrors.BlankAttribute("id"))
+            else
+                ValidationResult.ok()
+        }
 }
