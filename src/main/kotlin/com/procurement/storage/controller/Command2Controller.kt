a1e75dc49281a65367596476ad7cd04d42dd53f7
yevhenii <yevheniiskoryi@gmail.com>
2020-03-03 14:49:29 +0200

    ES-2522 updated controller to Result

--- src/main/kotlin/com/procurement/storage/controller/Command2Controller.kt
+++ src/main/kotlin/com/procurement/storage/controller/Command2Controller.kt
@@ -1,14 +1,18 @@
 package com.procurement.storage.controller
 
 import com.datastax.driver.core.querybuilder.QueryBuilder.toJson
-import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.storage.config.GlobalProperties
+import com.procurement.storage.domain.fail.Fail
+import com.procurement.storage.domain.util.Result
 import com.procurement.storage.infrastructure.web.dto.ApiResponse
 import com.procurement.storage.infrastructure.web.dto.ApiVersion
 import com.procurement.storage.model.dto.bpe.NaN
 import com.procurement.storage.model.dto.bpe.errorResponse
+import com.procurement.storage.model.dto.bpe.getAction
+import com.procurement.storage.model.dto.bpe.getId
+import com.procurement.storage.model.dto.bpe.getVersion
+import com.procurement.storage.model.dto.bpe.hasParams
 import com.procurement.storage.service.Command2Service
-import com.procurement.storage.utils.getBy
 import com.procurement.storage.utils.toNode
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
@@ -22,7 +26,7 @@ import java.util.*
 @RestController
 @RequestMapping("/command2")
 class Command2Controller(
-    private val command2Service:Command2Service
+    private val command2Service: Command2Service
 ) {
 
     companion object {
@@ -30,80 +34,49 @@ class Command2Controller(
     }
 
     @PostMapping
-    fun command(
-        @RequestBody requestBody: String
-    ): ResponseEntity<ApiResponse> {
-        log.info("Command request ($requestBody).")
+    fun command(@RequestBody requestBody: String): ResponseEntity<ApiResponse> {
         if (log.isDebugEnabled)
             log.debug("RECEIVED COMMAND: '${requestBody}'.")
 
-        val node: JsonNode = try {
-            requestBody.toNode()
-        } catch (expected: Exception) {
-            log.debug("Error.", expected)
-            return createErrorResponseEntity(
-                expected = expected
-            )
+        val node = when (val result = requestBody.toNode()) {
+            is Result.Success -> result.get
+            is Result.Failure -> return createErrorResponseEntity(expected = result.error)
         }
 
-        val id = try {
-            UUID.fromString(node.getBy(parameter = "id").asText())
-        } catch (expected: Exception) {
-            log.debug("Error.", expected)
-            return createErrorResponseEntity(
-                expected = expected
-            )
+        val id = when (val result = node.getId()) {
+            is Result.Success -> result.get
+            is Result.Failure -> return createErrorResponseEntity(expected = result.error)
         }
 
-        val version = try {
-            ApiVersion.valueOf(node.getBy(parameter = "version").asText())
-        } catch (expected: Exception) {
-            log.debug("Error.", expected)
-            return createErrorResponseEntity(
-                id = id,
-                expected = expected
-            )
+        val version = when (val result = node.getVersion()) {
+            is Result.Success -> result.get
+            is Result.Failure -> return createErrorResponseEntity(id = id, expected = result.error)
         }
 
-        try {
-            node.getBy(parameter = "params")
-        } catch (expected: Exception) {
-            log.debug("Error.", expected)
-            return createErrorResponseEntity(
-                id = id,
-                expected = expected,
-                version = version
-            )
+        when (val result = node.getAction()) {
+            is Result.Success -> Unit
+            is Result.Failure -> return createErrorResponseEntity(id = id, expected = result.error, version = version)
         }
 
-        val response = try {
-            command2Service.execute(request = node)
-                .also { response ->
-                    if (log.isDebugEnabled)
-                        log.debug("RESPONSE (id: '${id}'): '${toJson(response)}'.")
-                }
-        } catch (expected: Exception) {
-            log.debug("Error.", expected)
-            return createErrorResponseEntity(
-                id = id,
-                expected = expected,
-                version = version
-            )
-        }
+        val hasParams = node.hasParams()
+        if (hasParams.isError)
+            return createErrorResponseEntity(id = id, expected = hasParams.error, version = version)
 
-        return ResponseEntity(response , HttpStatus.OK)
+        val response = command2Service.execute(request = node)
+            .also { response ->
+                if (log.isDebugEnabled)
+                    log.debug("RESPONSE (id: '${id}'): '${toJson(response)}'.")
+            }
+        return ResponseEntity(response, HttpStatus.OK)
     }
 
     private fun createErrorResponseEntity(
-        expected: Exception,
+        expected: Fail,
         id: UUID = NaN,
-        version : ApiVersion = GlobalProperties.App.apiVersion
+        version: ApiVersion = GlobalProperties.App.apiVersion
     ): ResponseEntity<ApiResponse> {
-        val response = errorResponse(
-            exception = expected,
-            version = version,
-            id = id
-        )
+        log.debug("Error.", expected)
+        val response = errorResponse(exception = expected, version = version, id = id)
         return ResponseEntity(response, HttpStatus.OK)
     }
 }
