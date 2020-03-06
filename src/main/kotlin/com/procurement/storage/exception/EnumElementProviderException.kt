7941d45de8e3003064b4eff3a8e6ff2e729508a0
yevhenii <yevheniiskoryi@gmail.com>
2020-03-06 11:22:07 +0200

    ES-2522 added EnumElementProviderException

---
+++ src/main/kotlin/com/procurement/storage/exception/EnumElementProviderException.kt
@@ -0,0 +1,7 @@
+package com.procurement.storage.exception
+
+class EnumElementProviderException(enumType: String, value: String, values: String) : RuntimeException(
+    "Unknown value for enumType $enumType: $value, Allowed values are $values"
+) {
+    val code: String = "00.00"
+}
