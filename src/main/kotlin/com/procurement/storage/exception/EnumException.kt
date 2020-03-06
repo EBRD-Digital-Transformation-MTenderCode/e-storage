df56fde9f37d5e8c8aeb815c46b3ef13de4cab81
yevhenii <yevheniiskoryi@gmail.com>
2020-03-06 11:30:32 +0200

    ES-2522 removed EnumException

--- src/main/kotlin/com/procurement/storage/exception/EnumException.kt
+++
@@ -1,8 +0,0 @@
-package com.procurement.storage.exception
-
-data class EnumException(private val enumType: String, val value: String, val values: String) : RuntimeException(
-    "Unknown value for enumType $enumType: $value, Allowed values are $values"
-) {
-
-    val code: String = "00.00"
-}
